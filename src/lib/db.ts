5b997ecb8c5fee8cdb1aa01f02a778ec81767385
Jonathan Henrique de Araújo Silva <jonathandearaujosilva@gmail.com>
2025-02-01 16:14:23 -0300

    mode sqlite3 location to db subdirectory

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import Database from 'better-sqlite3';
 import path from 'path';
 
 // Define the database file path
-const dbPath = path.join(process.cwd(), 'database.sqlite');
+const dbPath = path.join(process.cwd(), 'db/database.sqlite');
 const db = new Database(dbPath, { verbose: console.log });
 
 export default db;
